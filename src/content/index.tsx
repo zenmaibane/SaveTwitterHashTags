5e5960e92163db7d2810fd437cab48d61e2fc8ad
tokei <koteicream.1024@gmail.com>
2024-09-24 22:38:22 +0900

    とにかくNを押したらタグが保持されるようになった

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -5,20 +5,21 @@ import { proxyStore } from '../app/proxyStore';
 proxyStore.ready().then(async () => {
   // Note:DOMが構築されるまで待つ
   await sleep(3000);
-  const tweetBoxClassName = '.public-DraftStyleDefault-block';
-  const tweetDom = document.querySelector(tweetBoxClassName);
-  console.log(tweetDom);
-  // debugger;
-  const postButtonDataTestId = 'tweetButtonInline';
-  // const tw = twind(config, sheet);
-  // observe(tw, shadowRoot);
-
-  // const shadowWrapper = document.createElement('div');
-  // shadowWrapper.id = 'root';
-  // shadowWrapper.style.display = 'contents';
-  // shadowRoot.appendChild(shadowWrapper);
-
-  console.log('あくあ色パレット');
+  const tweetButtonDom = document.querySelector<HTMLAnchorElement>(
+    'a[data-testid="SideNav_NewTweet_Button"] '
+  );
+  console.log(tweetButtonDom);
+  console.log('ファンタグレープ女');
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'n') {
+      console.log('n押したよ');
+      // 元あるNを押すとcomposeが開くやつをなくす
+      // TODO:今だとn押したら全部反応するからどうにか条件を作る
+      // TODO:検索がハッシュタグだったらという条件を作る
+      e.preventDefault();
+      tweetButtonDom?.click();
+    }
+  });
 });
 
 const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time)); //timeはミリ秒
